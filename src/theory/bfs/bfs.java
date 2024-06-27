f02be78c8c9e35c3f6b5ee3b23b4e2126e38c193
tahyo27 <tahyo27@gmail.com>
2024-06-27 22:28:25 +0900

    bfs

---
+++ src/theory/bfs/bfs.java
@@ -0,0 +1,32 @@
+package theory.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class bfs {
+    public static void main(String[] args) {
+        int[][] graph =  {{}, {2,3,8}, {1,6,8}, {1,5}, {5,7}, {3,4,7}, {2}, {4,5}, {1,2}};
+        boolean[] visited = new boolean[graph.length];
+
+        System.out.println(bfs(1, graph, visited));
+    }
+
+    static String bfs(int start, int[][] graph, boolean[] visited) {
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+        while(!q.isEmpty()) {
+            int nodeIdx = q.poll();
+            sb.append(nodeIdx).append(" >>>> ");
+            for(int i = 0; i < graph[nodeIdx].length; i++) {
+                int temp = graph[nodeIdx][i];
+                if(!visited[temp]) {
+                    visited[temp] = true;
+                    q.offer(temp);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
