df7acc42aab30d9257a8bf7a6703d0a1885dc663
tahyo27 <tahyo27@gmail.com>
2024-08-17 23:11:11 +0900

    baek 2750 버블 정렬

---
+++ src/baekjun/Baek2750.java
@@ -0,0 +1,26 @@
+package baekjun;
+
+import java.util.Scanner;
+
+public class Baek2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+        for(int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        for(int i = 0; i < N - 1; i++) {
+            for(int j = 0; j < N - 1 - i; j++) {
+                if(A[j] > A[j + 1]) {
+                    int tmp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = tmp;
+                }
+            }
+        }
+        for(int i = 0; i < N; i++) {
+            System.out.println(A[i]);
+        }
+    }
+}
