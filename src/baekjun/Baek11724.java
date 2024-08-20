ba73cd818b9e8d277541dfaa5b3f9c325a481569
tahyo27 <tahyo27@gmail.com>
2024-08-20 20:57:02 +0900

    baek11724 dfs

---
+++ src/baekjun/Baek11724.java
@@ -0,0 +1,50 @@
+package baekjun;
+import java.io.*;
+import java.util.*;
+
+public class Baek11724 {
+    static boolean[] visited;
+    static ArrayList<Integer>[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        visited = new boolean[n + 1];
+        list = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            list[s].add(e);
+            list[e].add(s);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(i);
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static void dfs(int n) {
+        if (visited[n]) {
+            return;
+        }
+        visited[n] = true;
+        for (int i : list[n]) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+}
