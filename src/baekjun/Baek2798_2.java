9db7ac62ecaf21b2fc3049c8babce322af3ce807
tahyo27 <tahyo27@gmail.com>
2024-10-17 22:25:01 +0900

    Baek 2798 브루트 투포인터

---
+++ src/baekjun/Baek2798_2.java
@@ -0,0 +1,48 @@
+package baekjun;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baek2798_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        Arrays.sort(array);
+
+        int result = 0;
+
+        // 첫 번째 숫자를 고정하고 나머지 두 숫자를 투포인터로 찾음
+        for (int i = 0; i < N - 2; i++) {
+            int left = i + 1;
+            int right = N - 1;
+
+            while (left < right) {
+                int sum = array[i] + array[left] + array[right];
+
+                if (sum == M) {
+                    result = sum;
+                    break;
+                }
+
+                if (sum < M) {
+
+                    result = Math.max(result, sum);
+                    left++;
+                } else {
+
+                    right--;
+                }
+            }
+        }
+        System.out.println(result);
+
+    }
+}
