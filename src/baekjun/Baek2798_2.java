e390725f152fdc7efd50a56932401b9b2722b5b7
tahyo27 <tahyo27@gmail.com>
2024-10-18 22:21:10 +0900

    Baek 2869

--- src/baekjun/Baek2798_2.java
+++ src/baekjun/Baek2798_2.java
@@ -18,8 +18,6 @@ public class Baek2798_2 {
         Arrays.sort(array);
 
         int result = 0;
-
-        // 첫 번째 숫자를 고정하고 나머지 두 숫자를 투포인터로 찾음
         for (int i = 0; i < N - 2; i++) {
             int left = i + 1;
             int right = N - 1;
